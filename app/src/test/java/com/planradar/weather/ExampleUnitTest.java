e2824b0b31fa7c10e681ba4dc7e4779e2c42bece
Eslam <a.aly.salhin@gmail.com>
2022-04-07 20:26:04 +0200

    unit Test for  Kelvin to Celsius function

--- app/src/test/java/com/planradar/weather/ExampleUnitTest.java
+++ app/src/test/java/com/planradar/weather/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package com.planradar.weather;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import com.planradar.weather.utils.KelvinToCelsius;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    static KelvinToCelsius toCelsius;
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @BeforeClass
+    public static void Beforerun(){
+        toCelsius=new KelvinToCelsius();
+    }
+    @Test
+    public void convertToCelsiusCase0() {
+        String result =toCelsius.kelvinToCelsius(301);
+        assertEquals("wrong Conversion", 28+" °C",result);
+    }
+    @Test
+    public void convertToCelsiusCase1() {
+        String result =toCelsius.kelvinToCelsius(300);
+        assertEquals( 27+" °C",result);
     }
 }
